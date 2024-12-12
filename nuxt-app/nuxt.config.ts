793731f07ce95ac97a09485e9d3a289357959425
Daniel Kelly <me@danielkelly.io>
2024-12-12 15:29:26 -0600

    wip

--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/icon', "@vueuse/nuxt"],
   ssr: false,
   app: {
     head: {
---
+++ nuxt-app/pages/18-debouncing-and-throttling-begin.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const searchTerm = ref('')
+
+const { data: result, execute } = await useFetch<{
+    products: any[]
+}>(
+    () => `https://dummyjson.com/products/search?q=${searchTerm.value}`
+)
+</script>
+<template>
+    <div class="p-4">
+        <input v-model="searchTerm" placeholder="Search products..." class="input input-bordered w-full mb-4" />
+        <div v-if="result?.products" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            <div v-for="product in result.products" :key="product.id" class="card bg-base-100 shadow-xl">
+                <figure><img :src="product.thumbnail" alt="Product Image" class="w-full h-48 object-cover" /></figure>
+                <div class="card-body">
+                    <h2 class="card-title">{{ product.title }}</h2>
+                    <p>{{ product.description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
